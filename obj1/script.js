33c877cf56dfdac79e05c381cb582436a51021d7
KissBenedek <95347210+KissBenedek@users.noreply.github.com>
2023-11-27 11:53:35 +0100

    Create script.js

---
+++ obj1/script.js
@@ -0,0 +1,9 @@
+let object = {firstName : "John", lastname: "Smith"}
+
+obj.firstName = "Paul";
+
+obj.firstName = "";
+
+function nev (){
+    console.log(obj.firstName + " " + obj.lastname + "" )
+}
